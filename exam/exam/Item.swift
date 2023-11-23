cc2e1c5cd93d91d1b55f392d43ff2031f46d2004
Erik Cabrera <a01334318@tec.mx>
2023-11-23 11:36:55 -0600

    add mvvm/clean architecture

---
+++ exam/exam/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  exam
+//
+//  Created by Erik Cabrera on 23/11/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
